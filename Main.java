1ca88952c3164fcafd7c2daa81a767bf2a521972
EA2311 <54818097+EA2311@users.noreply.github.com>
2019-12-01 21:26:32 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package onpu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Graph win = new Graph();
+        win.setVisible(true);
+        win.setResizable(false);
+    }
+}
